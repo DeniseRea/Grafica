cbc539ce2f78ca09729a8ef4067ff4bef55daf8c
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-06-17 11:59:32 -0500

    Cut Algorithm
    I should fix the bugs

---
+++ AlgoritmoRecorte/AlgoritmoRecorte/FrmRecorte.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AlgoritmoRecorte
+{
+    partial class FrmRecorte
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.graphic = new System.Windows.Forms.GroupBox();
+            this.dgwPixels = new System.Windows.Forms.DataGridView();
+            this.PicCanvas = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.BtnReset = new System.Windows.Forms.Button();
+            this.BtnCut = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.graphic.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPixels)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicCanvas)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dgwPixels);
+            this.groupBox1.Location = new System.Drawing.Point(683, 183);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(393, 386);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Salida de pixeles";
+            // 
+            // graphic
+            // 
+            this.graphic.Controls.Add(this.PicCanvas);
+            this.graphic.Location = new System.Drawing.Point(36, 31);
+            this.graphic.Name = "graphic";
+            this.graphic.Size = new System.Drawing.Size(634, 538);
+            this.graphic.TabIndex = 1;
+            this.graphic.TabStop = false;
+            this.graphic.Text = "Gráfica";
+            // 
+            // dgwPixels
+            // 
+            this.dgwPixels.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgwPixels.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgwPixels.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgwPixels.Location = new System.Drawing.Point(7, 22);
+            this.dgwPixels.Margin = new System.Windows.Forms.Padding(4);
+            this.dgwPixels.Name = "dgwPixels";
+            this.dgwPixels.RowHeadersWidth = 51;
+            this.dgwPixels.Size = new System.Drawing.Size(379, 357);
+            this.dgwPixels.TabIndex = 10;
+            // 
+            // PicCanvas
+            // 
+            this.PicCanvas.BackColor = System.Drawing.SystemColors.Control;
+            this.PicCanvas.Location = new System.Drawing.Point(7, 22);
+            this.PicCanvas.Margin = new System.Windows.Forms.Padding(4);
+            this.PicCanvas.Name = "PicCanvas";
+            this.PicCanvas.Size = new System.Drawing.Size(620, 509);
+            this.PicCanvas.TabIndex = 8;
+            this.PicCanvas.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.BtnReset);
+            this.groupBox2.Controls.Add(this.BtnCut);
+            this.groupBox2.Location = new System.Drawing.Point(698, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(371, 122);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Opciones";
+            // 
+            // BtnReset
+            // 
+            this.BtnReset.Location = new System.Drawing.Point(216, 47);
+            this.BtnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnReset.Name = "BtnReset";
+            this.BtnReset.Size = new System.Drawing.Size(100, 28);
+            this.BtnReset.TabIndex = 13;
+            this.BtnReset.Text = "Resetear";
+            this.BtnReset.UseVisualStyleBackColor = true;
+            this.BtnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // BtnCut
+            // 
+            this.BtnCut.Location = new System.Drawing.Point(54, 47);
+            this.BtnCut.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnCut.Name = "BtnCut";
+            this.BtnCut.Size = new System.Drawing.Size(100, 28);
+            this.BtnCut.TabIndex = 12;
+            this.BtnCut.Text = "Recortar";
+            this.BtnCut.UseVisualStyleBackColor = true;
+            this.BtnCut.Click += new System.EventHandler(this.BtnCut_Click);
+            // 
+            // FrmRecorte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 609);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.graphic);
+            this.Name = "FrmRecorte";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.graphic.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgwPixels)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PicCanvas)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgwPixels;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox graphic;
+        private System.Windows.Forms.PictureBox PicCanvas;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button BtnReset;
+        private System.Windows.Forms.Button BtnCut;
+    }
+}
+
---
+++ AlgoritmoRecorte/AlgoritmoRecorte/FrmRecorte.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlgoritmoRecorte
+{
+    public partial class FrmRecorte : Form
+    {
+        private CohenSutherland cohen;
+        private Point? firstClickPoint = null; // Punto del primer clic
+        private Point? tempPreviewPoint = null; // Punto temporal para vista previa
+
+        public FrmRecorte()
+        {
+            InitializeComponent();
+            cohen = new CohenSutherland(PicCanvas.ClientSize);
+            SetupDataGridView();
+
+            // Configurar eventos del PictureBox
+            PicCanvas.Paint += PicCanvas_Paint;
+            PicCanvas.MouseClick += PicCanvas_MouseClick;
+            PicCanvas.MouseMove += PicCanvas_MouseMove;
+        }
+
+        private void BtnCut_Click(object sender, EventArgs e)
+        {
+            cohen.ClipLines();
+            PicCanvas.Invalidate();
+            PopulateDataGridView();
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            PicCanvas.Image = null;
+            PicCanvas.Invalidate();
+            cohen = new CohenSutherland(PicCanvas.ClientSize);
+            dgwPixels.Rows.Clear();
+            firstClickPoint = null;
+            tempPreviewPoint = null;
+        }
+
+        private void PicCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            // Dibujar región de recorte
+            using (Pen clipPen = new Pen(Color.Red, 1))
+            {
+                g.DrawRectangle(clipPen, cohen.GetClipRegion());
+            }
+
+            // Dibujar las líneas almacenadas
+            cohen.Draw(g);
+
+            // Dibujar la cuadrícula
+            cohen.DrawGrid(g, PicCanvas.ClientSize);
+
+            // Dibujar línea de vista previa si estamos en el proceso de dibujar
+            if (firstClickPoint.HasValue && tempPreviewPoint.HasValue)
+            {
+                using (Pen previewPen = new Pen(Color.Gray, 1))
+                {
+                    g.DrawLine(previewPen, firstClickPoint.Value, tempPreviewPoint.Value);
+                }
+            }
+        }
+
+        private void PicCanvas_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                // Si no hay primer punto, guardarlo
+                if (!firstClickPoint.HasValue)
+                {
+                    firstClickPoint = e.Location;
+                }
+                // Si ya hay primer punto, añadir la línea y reiniciar
+                else
+                {
+                    cohen.AddLine(firstClickPoint.Value, e.Location);
+                    firstClickPoint = null; // Reiniciar para la próxima línea
+                    PicCanvas.Invalidate();
+                    PopulateDataGridView(); // Actualizar el DataGridView con la nueva línea
+                }
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                // Si hay un primer punto seleccionado, cancelar la operación
+                if (firstClickPoint.HasValue)
+                {
+                    firstClickPoint = null;
+                    PicCanvas.Invalidate();
+                }
+                // Si no, eliminar segmentos externos cercanos al punto
+                else if (cohen.RemoveExternalSegmentsAtPoint(e.Location))
+                {
+                    PicCanvas.Invalidate();
+                    PopulateDataGridView(); // Actualizar tras eliminar
+                }
+            }
+        }
+
+        private void PicCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Actualizar punto temporal para vista previa
+            if (firstClickPoint.HasValue)
+            {
+                tempPreviewPoint = e.Location;
+                PicCanvas.Invalidate();
+            }
+        }
+
+        private void SetupDataGridView()
+        {
+            dgwPixels.Columns.Clear();
+            dgwPixels.Columns.Add("StartPoint", "Punto Inicial");
+            dgwPixels.Columns.Add("EndPoint", "Punto Final");
+            dgwPixels.Columns.Add("BinaryValue", "Valor Binario");
+            dgwPixels.Columns.Add("OctantCode", "Código Octante");
+            dgwPixels.Columns.Add("RegionCode", "Código de Región");
+            dgwPixels.AllowUserToAddRows = false;
+        }
+
+        private void PopulateDataGridView()
+        {
+            dgwPixels.Rows.Clear();
+
+            var clippedLines = cohen.GetClippedLineSegments();
+            foreach (var line in clippedLines)
+            {
+                if (!line.start.IsEmpty && !line.end.IsEmpty)
+                {
+                    // Convertir las coordenadas a binario
+                    string startBinary = $"({Convert.ToString(line.start.X, 2)}, {Convert.ToString(line.start.Y, 2)})";
+                    string endBinary = $"({Convert.ToString(line.end.X, 2)}, {Convert.ToString(line.end.Y, 2)})";
+
+                    // Calcular código de octante
+                    int octantCode = CalculateOctantCode(line.start, line.end);
+                    string octantBinary = Convert.ToString(octantCode, 2).PadLeft(3, '0');
+
+                    // Obtener códigos de región (para el algoritmo de Cohen-Sutherland)
+                    string regionCodes = GetRegionCodes(line.start, line.end, cohen.GetClipRegion());
+
+                    dgwPixels.Rows.Add(
+                        $"({line.start.X}, {line.start.Y})",
+                        $"({line.end.X}, {line.end.Y})",
+                        $"Inicio: {startBinary}, Fin: {endBinary}",
+                        $"Octante: {octantCode} ({octantBinary})",
+                        regionCodes
+                    );
+                }
+            }
+        }
+
+        // Método para calcular el código de octante de una línea
+        private int CalculateOctantCode(Point start, Point end)
+        {
+            // Calcular las diferencias en X e Y
+            int dx = end.X - start.X;
+            int dy = end.Y - start.Y;
+
+            // Determinar el octante basado en el signo de dx, dy y sus magnitudes relativas
+            if (dx >= 0 && dy >= 0) // Primer cuadrante
+            {
+                if (dx >= dy) return 0;
+                else return 1;
+            }
+            else if (dx < 0 && dy >= 0) // Segundo cuadrante
+            {
+                if (-dx < dy) return 2;
+                else return 3;
+            }
+            else if (dx < 0 && dy < 0) // Tercer cuadrante
+            {
+                if (-dx >= -dy) return 4;
+                else return 5;
+            }
+            else // Cuarto cuadrante (dx >= 0 && dy < 0)
+            {
+                if (dx < -dy) return 6;
+                else return 7;
+            }
+        }
+
+        // Método para obtener códigos de región para el algoritmo Cohen-Sutherland
+        private string GetRegionCodes(Point start, Point end, Rectangle clipRect)
+        {
+            int startCode = ComputeOutCode(start, clipRect);
+            int endCode = ComputeOutCode(end, clipRect);
+
+            string startCodeBinary = Convert.ToString(startCode, 2).PadLeft(4, '0');
+            string endCodeBinary = Convert.ToString(endCode, 2).PadLeft(4, '0');
+
+            return $"Inicio: {startCodeBinary}, Fin: {endCodeBinary}";
+        }
+
+        // Método para calcular el código de región según Cohen-Sutherland
+        private int ComputeOutCode(Point p, Rectangle clipRect)
+        {
+            int code = 0;
+
+            if (p.X < clipRect.Left)           // a la izquierda
+                code |= 1;
+            else if (p.X > clipRect.Right)     // a la derecha
+                code |= 2;
+
+            if (p.Y < clipRect.Top)            // arriba
+                code |= 4;
+            else if (p.Y > clipRect.Bottom)    // abajo
+                code |= 8;
+
+            return code;
+        }
+    }
+}
